67d82e2cf88dd61f856054000b772f18127fdc2b
domdom1524 <134648962+domdom1524@users.noreply.github.com>
2023-05-26 13:37:24 +0100

    Add files via upload

---
+++ melton quiz.py
@@ -0,0 +1,13 @@
+def are_you_a_melton
+    print("in this quiz we will find out if you are the  perfect melton")
+    name=input("please enter you name ")
+    print("well hello",name,"arent you brave")
+    melton=input("here is your first question... do you like lazers  y or n")
+    if melton=="y":
+        print("well well")
+    elif melton=="n":
+        print("not who i thought you where")
+    else:
+        print("you dont understand maybe you are a melton")
+        
+    nt=input("do you enjoy im a celeb y or n ")
